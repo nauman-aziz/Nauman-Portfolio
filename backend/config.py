45a8774828e60f3f698be0d47c2a01e5e6fca49e
Nauman Aziz <mnauman@quantumgears.com>
2025-04-13 20:59:45 +0500

    backend URL fix for assests deployments

--- backend/config.py
+++ backend/config.py
@@ -2,12 +2,12 @@ import os
 
 class Config:
     # Default base URL, can be overridden by specific configurations.
-    BASE_URL = os.environ.get("BASE_URL", "https://my-portfolio-backend-ten.vercel.app/")
+    BASE_URL = os.environ.get("BASE_URL", "https://nauman-portfolio-flask-backend.vercel.app/")
 
 class DevelopmentConfig(Config):
     BASE_URL = "http://localhost:5000"
     DEBUG = True  # Additional development configurations can be added.
 
 class ProductionConfig(Config):
-    BASE_URL = "https://my-portfolio-backend-ten.vercel.app/"
+    BASE_URL = "https://nauman-portfolio-flask-backend.vercel.app/"
     DEBUG = False
